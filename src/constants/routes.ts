b1d6e535a562eaa1dc774e2812c4a73165ec1cdb
Ivan Lytovka <ivanlytovka@gmail.com>
2021-11-13 20:24:28 +0200

    test mui and add route constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const HOMEPAGE_ROUTE = '/';
+export const LEADER_BOARD_ROUTE = '/leader-board';
+export const LOGIN_ROUTE = '/login';
+export const QUIZ_SESSION_ROUTE = '/quiz-session';
+export const QUIZ_SETUP_ROUTE = '/quiz-setup';
+export const WIKI_PAGE_ROUTE = '/wiki/:page';
+export const WIKI_ROUTE = '/wiki';
