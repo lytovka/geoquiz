179c87312ae9b160d0944f9f091ed5532e24b71c
msrosh <msrosh@scs.ryerson.ca>
2021-11-27 17:09:44 -0500

    edit for MediaAnswerCard to return proper answers, both now work similarly

--- src/components/answer-card/answer-card/MediaAnswerCard.tsx
+++ src/components/answer-card/answer-card/MediaAnswerCard.tsx
@@ -16,57 +16,54 @@ export const MediaAnswerCard = ({
   answer2,
   answer3,
   answer4,
-  correct
-}: IAnswerCardProps): JSX.Element => {
-  //const [answerID, setAnswerID] = React.useState(-1);
-  //const answers = [{ _id: "1" }, { _id: "2" }, { _id: "3" }, { _id: "4" }];
+  correct,
+  score
+}: IAnswerCardProps): [JSX.Element, number] => {
+  const answers = [false, false, false, false];
+  answers[correct-1] = true;
+  function response(x: boolean, y:number): number {
+    if (x) {return y++}
+    else return y
+  }
   //Alternate version of @AnswerCardProps, can use images instead if given proper name of file in question field
   const ques = question.concat(".svg");
-  return (
+  return [
     <Fragment>
       <Card sx={{ maxWidth: 235, bgcolor: "lightgray", padding: 1, fontFamily:"helvetica", fontWeight:"bold", textAlign:"center" , fontSize:"20px"}}>
         <CardMedia component="img" src={ques} />
         <Grid container spacing={0} gap={1}>
           <Grid container spacing={0} gap={2} sx={{ margin: 2 }}>
             <Card sx={{ maxWidth: 345, bgcolor:"orange", fontFamily:"helvetica" }}>
-              <CardActionArea>
+              <CardActionArea
+              onClick={() => response(answers[0], score)}>
                 <CardContent>
                   <Typography sx={{ fontFamily:"helvetica"}}>{answer1}</Typography>
-                  <Collapse>
-                    <CardContent>{correct}</CardContent>
-                  </Collapse>
                 </CardContent>
               </CardActionArea>
             </Card>
             <Card sx={{ maxWidth: 345, bgcolor:"orange" }}>
-              <CardActionArea>
+              <CardActionArea
+              onClick={() => response(answers[1], score)}>
                 <CardContent>
                   <Typography>{answer2}</Typography>
-                  <Collapse>
-                    <CardContent>{correct}</CardContent>
-                  </Collapse>
                 </CardContent>
               </CardActionArea>
             </Card>
           </Grid>
           <Grid container spacing={0} gap={2} sx={{ ml: 2, mb: 2, mr: 2 }}>
             <Card sx={{ maxWidth: 345, bgcolor:"orange" }}>
-              <CardActionArea>
+              <CardActionArea
+              onClick={() => response(answers[2], score)}>
                 <CardContent>
                   <Typography>{answer3}</Typography>
-                  <Collapse>
-                    <CardContent>{correct}</CardContent>
-                  </Collapse>
                 </CardContent>
               </CardActionArea>
             </Card>
             <Card sx={{ maxWidth: 345, bgcolor:"orange" }}>
-              <CardActionArea>
+              <CardActionArea
+              onClick={() => response(answers[3], score)}>
                 <CardContent>
                   <Typography>{answer4}</Typography>
-                  <Collapse>
-                    <CardContent>{correct}</CardContent>
-                  </Collapse>
                 </CardContent>
               </CardActionArea>
             </Card>
@@ -74,5 +71,5 @@ export const MediaAnswerCard = ({
         </Grid>
       </Card>
     </Fragment>
-  );
+  , score]
 };
