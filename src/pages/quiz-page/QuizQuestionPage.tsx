b982f0c6d89540e3b1b71c3015d4de3df58ab621
msrosh <msrosh@scs.ryerson.ca>
2021-12-01 00:07:46 -0500

    removes error that i left in

--- src/pages/quiz-page/QuizQuestionPage.tsx
+++ src/pages/quiz-page/QuizQuestionPage.tsx
@@ -3,11 +3,11 @@ import {Grid} from '@mui/material';
   
 const score = 0;
 //not sure how to write this properly
-const q1 = [<AnswerCard question="What is the Capital of Belarus?" answer1="Minsk" answer2="Paris" answer3="Ottawa" answer4="Cyprus" score={score} correct={1}/>, score]
+const q1 = <AnswerCard question="What is the Capital of Belarus?" answer1="Minsk" answer2="Paris" answer3="Ottawa" answer4="Cyprus" score={score} correct={1}/>
 export const QuizQuestionPage = () => {
     return (
         <Grid container spacing={0} alignItems="center" justifyContent="center">
-            {q1[0]}
+            {q1}
         </Grid>
     )
 }
